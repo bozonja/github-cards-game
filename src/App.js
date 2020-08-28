5ca55612fff0b8d118647e15c7dd3c9bfae81ba8
Josip Markovic <bozadesign@gmail.com>
2020-08-28 15:51:58 +0200

    Move content for receiving it as a prop

--- src/App.js
+++ src/App.js
@@ -3,12 +3,30 @@ import './App.css';
 import Cardlist from './components/Cardlist';
 import Form from './components/Form';
 
+const githubData = [
+  {
+    name: "Josip Marković", 
+    bio: "Designer & FrontEnd Developer",
+    avatar_url: "https://avatars0.githubusercontent.com/u/35841169?v=4"
+  },
+  {
+    name: "Viktor Lešić", 
+    bio: "FrontEnd Developer",
+    avatar_url: "https://avatars1.githubusercontent.com/u/32477795?v=4"
+  },
+  {
+    name: "Toni Franulic", 
+    bio: "FrontEnd Developer",
+    avatar_url: "https://avatars2.githubusercontent.com/u/38908260?v=4"
+  }
+];
+
 function App() {
   return (
     <div className="App">
       <h1 className="header-title">Github profiles</h1>
       <Form />
-      <Cardlist />
+      <Cardlist profiles={githubData}/>
     </div>
   );
 }
--- src/components/Cardlist.js
+++ src/components/Cardlist.js
@@ -1,27 +1,9 @@
 import React from 'react';
 import Card from './Card';
 
-const githubData = [
-  {
-    name: "Josip Marković", 
-    bio: "Designer & FrontEnd Developer",
-    avatar_url: "https://avatars0.githubusercontent.com/u/35841169?v=4"
-  },
-  {
-    name: "Viktor Lešić", 
-    bio: "FrontEnd Developer",
-    avatar_url: "https://avatars1.githubusercontent.com/u/32477795?v=4"
-  },
-  {
-    name: "Toni Franulic", 
-    bio: "FrontEnd Developer",
-    avatar_url: "https://avatars2.githubusercontent.com/u/38908260?v=4"
-  }
-];
-
-const Cardlist = () => (
+const Cardlist = (props) => (
   <div>
-     {githubData.map(profile => <Card {...profile}/>)}
+     {props.profiles.map(profile => <Card {...profile}/>)}
   </div>
 );
 
